fc04d64b6674685b9166569f03197dc0d04f0b2b
HP <HP@LAPTOP-OAI0VABU>
2022-03-01 08:56:21 +0530

    My first commit of framework

---
+++ Testactitime/src/testPackage/TestActiTimeLogout.java
@@ -0,0 +1,33 @@
+package testPackage;
+
+import java.io.IOException;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import generic.BaseTest;
+import generic.CustomListner;
+import generic.Flib;
+import pagePackage.HomePage;
+import pagePackage.LoginPage;
+@Listeners(CustomListner.class)
+public class TestActiTimeLogout extends BaseTest{
+
+	@Test
+	public void testActiTimeLogout() throws IOException
+	{	
+		LoginPage lp = new LoginPage(driver);
+		Flib flib = new Flib();
+		String validUsername = flib.readPropertyData(PROP_PATH, "un");
+		String validPassword = flib.readPropertyData(PROP_PATH, "pass");
+
+		lp.validLogin(validUsername, validPassword);
+
+		HomePage hp = new HomePage(driver);
+		hp.logout();
+
+
+	}
+
+
+}
---
+++ Testactitime/src/testPackage/TestActiTimeValidLogin.java
@@ -0,0 +1,39 @@
+package testPackage;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import generic.BaseTest;
+import generic.CustomListner;
+import generic.Flib;
+import generic.WebDriverCommonLib;
+
+import pagePackage.LoginPage;
+@Listeners(CustomListner.class)
+public class TestActiTimeValidLogin extends BaseTest {
+	
+	
+	@Test
+	public void testActiTimeValidlogin() throws IOException, InterruptedException
+	{
+		LoginPage lp = new LoginPage(driver);
+		Flib flib = new Flib();
+		String validUsername = flib.readPropertyData(PROP_PATH, "un");
+		String validPassword = flib.readPropertyData(PROP_PATH, "pass");
+		
+		lp.validLogin(validUsername, validPassword);
+		
+		WebDriverCommonLib wb = new WebDriverCommonLib();
+		String actualHomePageTitle = wb.getTheTitleOfTheWebPage();
+		Thread.sleep(2000);
+		Assert.assertEquals(actualHomePageTitle, "suraj");
+		
+			
+	}
+
+	
+	
+}
